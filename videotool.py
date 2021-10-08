e04f0d565873a4c03540bba2e734e8537a9e9066
Qiwichupa <qiwichupa@gmail.com>
2021-10-09 00:54:23 +0300

    +merge

--- videotool.py
+++ videotool.py
@@ -4,7 +4,7 @@ import argparse
 import ffmpeg
 import glob
 import os
-
+import subprocess
 
 
 
@@ -15,7 +15,7 @@ def resize_single_video():
     for i in range(len(ffmpeg.probe(infile)['streams'])):
         if ffmpeg.probe(infile)['streams'][i]['codec_type'] == 'video':
             videoinfo = ffmpeg.probe(infile)['streams'][i]
-            break;
+            break
     height = videoinfo['height']
     width = videoinfo['width']
 
@@ -23,12 +23,13 @@ def resize_single_video():
     audio = input.audio
     video = input.video
 
-    if args.scale!=1:
-        newwidth = width*args.scale
-        newheight = height*args.scale
+    if args.s != 1:
+        newwidth = int(float(width*args.s))
+        newheight = int(float(height*args.s))
         video = video.filter('scale', width=newwidth, height=newheight)
-    elif args.resolution:
-        video = video.filter('scale', width=args.width, height=args.height)
+    elif args.r:
+        width, height = args.r.split('x')
+        video = video.filter('scale', width=int(float(width)), height=int(float(height)))
     out = ffmpeg.output(audio, video, outfile, qscale=0)
     out.run()
 
@@ -81,15 +82,7 @@ def convert_to_gif():
     input = ffmpeg.input(infile)
     video = input.video
 
-    #optional scaling
-    if args.scale!=1:
-        newwidth = width*args.scale
-        newheight = height*args.scale
-        video = video.filter('scale', width=newwidth, height=newheight)
-    elif args.resolution:
-        video = video.filter('scale', width=args.width, height=args.height)
-
-    video = video.filter('fps', args.fps)
+    video = video.filter('fps', args.x)
     out = ffmpeg.output(video, outfile, loop=0)
     out.run()
 
@@ -141,6 +134,44 @@ def convert_to_x264():
                     out = ffmpeg.output(video, outfile, qscale=0)
                 out.run()
 
+def avmerge(files, maxWidth=1920, maxHeight=1080, frameRate=30, ffmpegcmd='ffmpeg'):
+    frameRate = str(frameRate)
+    maxWidth = str(maxWidth)
+    maxHeight = str(maxHeight)
+
+    cmdoptions = []
+    filteropt1 = ''
+    filteropt2 = ''
+    for i, file in enumerate(files):
+        cmdoptions += ['-i']
+        cmdoptions += [file]
+        filteropt1 = '{filteropt1}[{i}:v]scale=iw*sar*min({maxWidth}/(iw*sar)\,{maxHeight}/ih):ih*min({maxWidth}/(iw*sar)\,{maxHeight}/ih),pad={maxWidth}:{maxHeight}:(ow-iw)/2:(oh-ih)/2,setsar=1[{i}v];'.format(
+            filteropt1=filteropt1, i=i, maxWidth=maxWidth, maxHeight=maxHeight)
+        filteropt2 = '{filteropt2}[{i}v] [{i}:a] '.format(filteropt2=filteropt2, i=i)
+    filteropt3 = 'concat=n={}:v=1:a=1 [v] [a]'.format(len(files))
+    filter = '{}{}{}'.format(filteropt1, filteropt2, filteropt3)
+
+    convertCmdString = [ffmpegcmd] + cmdoptions + ['-filter_complex'
+        , filter
+        , '-map'
+        , '[v]'
+        , '-map'
+        , '[a]'
+        , '-c:a', 'libmp3lame'
+        , '-ar', '48000'
+        , '-c:v', 'libx264'
+        , '-r', frameRate
+        , '-bf', '2'
+        , '-g', frameRate
+        , '-profile:v', 'high'
+        , '-preset', 'fast'
+        , '-level', '42'
+        , 'output.mp4'
+                                                   ]
+    print(convertCmdString)
+    output = subprocess.Popen(convertCmdString).communicate()
+    print(output)
+
 def convert_to_mp3():
     infile = args.inpt
     outfile = os.path.splitext(infile)[0] + '.mp3'
@@ -164,20 +195,19 @@ if __name__ == '__main__':
     actions.add_argument('--towebm', action='store_true', help='convert single video to webm')
     actions.add_argument('--tomp3', action='store_true', help='extract audio to mp3')
 
-    resizeMethod1 = parser.add_argument_group('resize method #1 (used with: --resize, --togif)')
-    resizeMethod1.add_argument('--scale', type=float,  default=1, metavar='S', help='scale multiplier: 0.5, 2, 3.4, etc')
-    
-    resizeMethod2 = parser.add_argument_group('resize method #2 (used with: --resize, --togif)')
-    resizeMethod2.add_argument('--resolution', action='store_true', help='change resolution. Use with --width and(or) --height')
-    resizeMethod2.add_argument('--width', '-x', type=int,  default=-1, metavar='X', help='outfile width')
-    resizeMethod2.add_argument('--height', '-y', type=int,  default=-1, metavar='Y', help='outfile height')
+    mergeParams = parser.add_argument_group('--merge options:')
+    mergeParams.add_argument('-f', type=str,  default=None, metavar='F',  help='video format string: 1024x768@30')
+
+    resizeParams = parser.add_argument_group('--resize options: -s OR -r ')
+    resizeParams.add_argument('-s', type=float,  default=1, metavar='S', help='scale multiplier: 0.5, 2, 3.4, etc')
+    resizeParams.add_argument('-r', type=str,  default=None, metavar='R',  help='set resolution, ex.: 1280x720')
 
     cutParams = parser.add_argument_group('--cut options')
     cutParams.add_argument('-a', type=str, default='-1', help='start point in [HH:][MM:]SS[.m...] format')
     cutParams.add_argument('-b', type=str, default='-1', help='end point  in [HH:][MM:]SS[.m...] format')
 
     togifParams = parser.add_argument_group('--togif options')
-    togifParams.add_argument('--fps', type=int,  default=10, help='fps for gif (default: 10)')
+    togifParams.add_argument('-x', type=int, default=10, metavar='X', help='fps for gif (default: 10)')
 
     to264Params = parser.add_argument_group('--to264 options')
     to264Params.add_argument('--force', action='store_true', help='reencode h264 video')
@@ -199,3 +229,16 @@ if __name__ == '__main__':
         cut_single_video()
     elif args.tomp3:
         convert_to_mp3()
+    elif args.merge:
+        files = []
+        for file in sorted(os.listdir('.')):
+            if not file.endswith(args.inpt):
+                continue
+            files.append(file)
+        resolution, *fps = args.f.split('@')
+        if len(fps) < 1:
+            fps = 30
+        else:
+            fps = int(fps[0])
+        width, height = resolution.split('x')
+        avmerge(files, int(width), int(height), int(fps))
