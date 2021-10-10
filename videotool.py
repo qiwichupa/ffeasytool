709948420d4b249359f5bbb4ac74f6bbf2a92f2b
Qiwichupa <qiwichupa@gmail.com>
2021-10-10 03:31:22 +0300

    moving to class and direct ffmpeg

--- videotool.py
+++ videotool.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 import argparse
-import ffmpeg
 import glob
 import os
 import subprocess
@@ -132,6 +131,7 @@ class VideoTool():
         if out.strip() == 'vp8':
             print('"{}" is already webm, skipped.'.format(infile))
             return
+
         cmd = [self.bins['ffmpeg']
             , '-i'
             , infile
@@ -141,118 +141,124 @@ class VideoTool():
             , outfile]
         subprocess.Popen(cmd).communicate()
 
+    def convert_to_x264(self, infile, outfile):
 
-def convert_to_x264():
-    ext = args.name
-    for f in os.listdir('.'):
-        if f.lower().endswith(ext.lower()):
-            outfile = os.path.splitext(f)[0] + '_converted.mp4'
-            for i in range(len(ffmpeg.probe(f)['streams'])):
-                if ffmpeg.probe(f)['streams'][i]['codec_type'] == 'video':
-                    videoinfo = ffmpeg.probe(f)['streams'][i]
-                    break;
-            if videoinfo['codec_name'] != 'h264' or args.force:
-                input = ffmpeg.input(f)
-                video = input.video
-                audio = None
-                # searching audio
-                for i in range(len(ffmpeg.probe(f)['streams'])):
-                    if ffmpeg.probe(f)['streams'][i]['codec_type'] == 'audio':
-                        audio = input.audio
-                        break;
-                if audio is not None:
-                    out = ffmpeg.output(audio, video, outfile, qscale=0)
-                else:
-                    out = ffmpeg.output(video, outfile, qscale=0)
-                out.run()
+        # check video codec
+        cmd = [self.bins['ffprobe']
+            , '-v', 'error'
+            , '-select_streams', 'v:0'
+            , '-show_entries', 'stream=codec_name'
+            , '-of', 'default=noprint_wrappers=1:nokey=1'
+            , infile]
+        out, err = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True).communicate()
 
+        if out.strip() == 'h264':
+            print('"{}" is already h264, skipped.'.format(infile))
+            return
 
-def convert_to_mp3():
-    infile = args.name
-    outfile = os.path.splitext(infile)[0] + '.mp3'
+        cmd = [self.bins['ffmpeg']
+            , '-i'
+            , infile
+            , '-c:v', 'libx264'
+            , '-profile:v', 'high'
+            , '-preset', 'fast'
+            , '-level', '42'
+            , outfile]
+        subprocess.Popen(cmd).communicate()
 
-    input = ffmpeg.input(infile)
-    audio = input.audio
-
-    out = ffmpeg.output(audio, outfile)
-    out.run()
+    def convert_to_mp3(self, infile, track=0, outfile='out.mp3'):
+        cmd = [self.bins['ffmpeg']
+            , '-i'
+            , infile
+            , '-map', '0:a:{}'.format(track)
+            , '-c:a', 'libmp3lame'
+            , '-ar', '48000'
+            , outfile]
+        subprocess.Popen(cmd).communicate()
 
 
 if __name__ == '__main__':
+    appname = 'vi'
     parser = argparse.ArgumentParser()
 
     actions = parser.add_argument_group('MAIN ACTIONS')
-    actions.add_argument('--merge', action='store_true', help='merge video files')
-    mergeParams = parser.add_argument_group('--merge options:')
+    actions.add_argument('--merge', action='store_true', help='''merge video files (use quoted wildcards). Ex.: "{n} --merge -f 1280x720 'my*.mp4'" '''.format(n=parser.prog))
+    mergeParams = parser.add_argument_group('--merge options')
     mergeParams.add_argument('-f', type=str, default=None, metavar='F', help='video format string: 1280x720[@30]')
 
-    actions.add_argument('--resize', action='store_true', help='resize single video')
-    resizeParams = parser.add_argument_group('--resize options: -s OR -r ')
+    actions.add_argument('--resize', action='store_true', help='''resize single video. Ex.: "{n} --resize -m 0.5 myvideo.mp4",  "{n} --resize -r 1280x720 myvideo.mp4"'''.format(n=parser.prog))
+    resizeParams = parser.add_argument_group('--resize options (use -m or -r)')
     resizeParams.add_argument('-m', type=float, default=1, metavar='M', help='multiplier: 0.5, 2, 3.4, etc.')
     resizeParams.add_argument('-r', type=str, default=None, metavar='R', help='resolution: 1280x720, etc.')
 
-    actions.add_argument('--cut', action='store_true', help='cut single video. Use -a and(or) -b ')
-    cutParams = parser.add_argument_group('--cut options')
+    actions.add_argument('--cut', action='store_true', help='''cut single video. Use -a and(or) -b parameters as 
+                                                                                            start and end points. Ex.: "{n} --cut -a 01:05 -b 02:53 myvideo.mp4" '''.format(n=parser.prog))
+    cutParams = parser.add_argument_group('--cut options (use -a and(or) -b)')
     cutParams.add_argument('-a', type=str, default='-1', help='start point in [HH:][MM:]SS[.m...] format')
     cutParams.add_argument('-b', type=str, default='-1', help='end point  in [HH:][MM:]SS[.m...] format')
 
-    actions.add_argument('--togif', action='store_true', help='convert single video to gif')
-    togifParams = parser.add_argument_group('--togif options')
+    actions.add_argument('--togif', action='store_true', help='''convert file(s) to gif. Ex.: "{n} --togif -x 5  'my*.mp4'" '''.format(n=parser.prog))
+    togifParams = parser.add_argument_group('--togif options (optional)')
     togifParams.add_argument('-x', type=int, default=10, metavar='X', help='fps for gif (default: 10)')
 
-    actions.add_argument('--to264', action='store_true', help='convert all files with specified extension to mp4/h264. H264-video will be skipped')
-    to264Params = parser.add_argument_group('--to264 options')
-    to264Params.add_argument('--force', action='store_true', help='reencode h264 video')
+    actions.add_argument('--to264', action='store_true', help='''convert file(s) to mp4/h264. Ex.: "{n} --to264  'my*.wmv'" '''.format(n=parser.prog))
+    actions.add_argument('--towebm', action='store_true', help='''convert file(s) to webm. Ex.: "{n} --towebm  'my*.mp4'" '''.format(n=parser.prog))
+    actions.add_argument('--tomp3', action='store_true', help='''extract audio to mp3. Ex.: "{n} --tomp3 -t 2  'my*.mp4'" '''.format(n=parser.prog))
+    tomp3Params = parser.add_argument_group('--tomp3 options (optional)')
+    tomp3Params.add_argument('-t', type=int, default=1, metavar='T', help='track (default: 1)')
 
-    actions.add_argument('--towebm', action='store_true', help='convert single video to webm')
-    actions.add_argument('--tomp3', action='store_true', help='extract audio to mp3')
-
-    parser.add_argument('name', type=str, help='file or extension to operate')
+    parser.add_argument('name', type=str, help='''filename or quoted wildcards (myvideo.mp4, 'vid*.mp4', etc.). 
+                                                                                Wildcards MUST be used with --merge key, or CAN be used with --to* keys.''')
 
     args = parser.parse_args()
 
+    files = []
+    for f in sorted(glob.glob(args.name)):
+        files.append(f)
+
     videotool = VideoTool()
+
     if args.resize:
-        infile = args.name
+        infile = files[0]
         outfile = '{}_resized.mp4'.format(os.path.splitext(args.name)[0])
         if args.m != 1:
             videotool.resize_single_video(infile=infile, scale=args.m, outfile=outfile)
         elif args.r:
             videotool.resize_single_video(infile=infile, resolution=args.r, outfile=outfile)
-        # resize_single_video()
     elif args.togif:
-        infile = args.name
-        outfile = '{}.gif'.format(os.path.splitext(args.name)[0])
-        fps = args.x
-        videotool.convert_to_gif(infile, fps, outfile)
+        for infile in files:
+            outfile = '{}.gif'.format(os.path.splitext(infile)[0])
+            fps = args.x
+            videotool.convert_to_gif(infile, fps, outfile)
     elif args.to264:
-        convert_to_x264()
+        for infile in files:
+            outfile = '{}.mp4'.format(os.path.splitext(infile)[0])
+            videotool.convert_to_x264(infile, outfile)
     elif args.towebm:
-        infile = args.name
-        outfile = '{}.webm'.format(os.path.splitext(args.name)[0])
-        videotool.convert_to_webm(infile, outfile)
+        for infile in files:
+            outfile = '{}.webm'.format(os.path.splitext(infile)[0])
+            videotool.convert_to_webm(infile, outfile)
     elif args.cut:
-        infile = args.name
-        outfile = '{}_cut.mp4'.format(os.path.splitext(args.name)[0])
+        infile = files[0]
+        outfile = '{}_cut.mp4'.format(os.path.splitext(infile)[0])
         if args.a == -1 and args.b == -1:
             print('use -a and(or) -b')
         else:
             videotool.cut_single_video(infile=infile, startpoint=args.a, endpoint=args.b, outfile=outfile)
     elif args.tomp3:
-        convert_to_mp3()
+        tracknum = args.t - 1
+        for infile in files:
+            outfile = '{}.mp3'.format(os.path.splitext(infile)[0])
+            videotool.convert_to_mp3(infile, tracknum, outfile)
     elif args.merge:
         if args.f is None:
             print('use -f to set resolution (-f 1280x720[@30])')
             exit(1)
-        files = []
-        for file in sorted(os.listdir('.')):
-            if not file.endswith(args.name):
-                continue
-            files.append(file)
+        filestomerge = files
         resolution, *fps = args.f.split('@')
         if len(fps) < 1:
             fps = 30
         else:
             fps = int(fps[0])
         width, height = resolution.split('x')
-        videotool.avmerge(files, int(width), int(height), int(fps), outfile='myout.mp4')
+        videotool.avmerge(filestomerge, int(width), int(height), int(fps), outfile='myout.mp4')
